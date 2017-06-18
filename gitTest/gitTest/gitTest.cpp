1900d690a26e8fc6b8f5e915b47987571426fe3d
JingweiGuo <173522770@qq.com>
2017-06-18 19:16:10 +0800

    fist version

---
+++ gitTest/gitTest/gitTest.cpp
@@ -0,0 +1,53 @@
+// gitTest.cpp : 定义控制台应用程序的入口点。
+//
+
+
+#include "stdafx.h"
+#include <vector>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+int candy(vector<int> &ratings)
+{
+	int n = ratings.size();
+	vector<int> increment(n);
+
+	for (int i = 1, inc = 1; i < n; i++)
+	{
+		if (ratings[i]>ratings[i - 1])
+			increment[i] = max(inc++, increment[i]);
+		else
+		{
+			inc = 1;
+		}
+	}
+	for (int i = n - 2, inc = 1; i >= 0; i--)
+	{
+		if (ratings[i]>ratings[i + 1])
+			increment[i] = max(inc++, increment[i]);
+		else
+		{
+			inc = 1;
+		}
+	}
+
+	int sum = n;
+
+	for (size_t i = 0; i < n; i++)
+	{
+		sum += increment[i];
+	}
+	return sum;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a[4] = { 7, 2, 11, 15 };
+	vector<int> num(a, a + 4);
+	int result = candy(num);
+	return 0;
+}
+
+
+
